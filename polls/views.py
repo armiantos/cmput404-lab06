6a0554f66a7cfd116ad74b030889e38eb4f9d5dc
Armianto Sumitro <armiantos@gmail.com>
2022-01-31 18:10:28 -0700

    Fix increment votes

--- polls/views.py
+++ polls/views.py
@@ -31,6 +31,6 @@ def vote(request, question_id):
             'error_message': 'You did not select a choice.'
         })
     else:
-        selected_choice.votes = + 1
+        selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
